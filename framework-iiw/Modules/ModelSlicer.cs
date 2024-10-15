4c666e7e236d3a71fc88159f382489d630480ac5
jmboy01 <jan-mathijs.pex@student.uhasselt.be>
2024-10-15 20:07:18 +0200

    First commit

---
+++ framework-iiw/Modules/ModelSlicer.cs
@@ -0,0 +1,235 @@
+﻿using Clipper2Lib;
+using framework_iiw.Data_Structures;
+using framework_iiw.Exceptions;
+using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Printing.IndexedProperties;
+using System.Windows.Media.Media3D;
+using System.Windows.Media.TextFormatting;
+using LineSegment = framework_iiw.Data_Structures.LineSegment;
+using SlicerSettings = framework_iiw.Settings.Settings;
+
+
+namespace framework_iiw.Modules
+{
+    class ModelSlicer
+    {
+        GeometryModel3D? geometryModel3D;
+
+        private double sliderValue = 0;
+
+        public ModelSlicer() {}
+    
+        // --- Slice
+
+        public List<PathsD> Slice()
+        {
+            if (geometryModel3D == null) { throw new NullException("Geometry Model Must Be Loaded First"); }
+
+            MeshGeometry3D meshGeometry3D = ModelLoader.LoadMesh(geometryModel3D);
+
+            List<int> triangleIndices = meshGeometry3D.TriangleIndices.ToList();
+            List<Point3D> positions = meshGeometry3D.Positions.ToList();
+
+            var layers = new List<PathsD>();
+            var totalAmountOfLayers  = geometryModel3D.Bounds.SizeZ / SlicerSettings.LayerHeight;
+
+            for (var idx = 0; idx < totalAmountOfLayers; idx++)
+            {
+                var layer = SliceModelAtSpecificLayer(idx * SlicerSettings.LayerHeight, meshGeometry3D, triangleIndices, positions);
+
+                layers.Add(layer);
+            }
+
+            // var layer = SliceModelAtSpecificLayer(sliderValue, meshGeometry3D, triangleIndices, positions);
+            // var layers = new List<PathsD>{ layer };
+            return layers;
+        }
+
+        // --- Slice Object At Specific Layer
+
+        private PathsD SliceModelAtSpecificLayer(double layer, MeshGeometry3D meshGeometry, List<int> triangleIndices, List<Point3D> positions)
+        {
+            var slicingPlaneHeight = GetSlicingPlaneHeight(meshGeometry.Bounds.Z, layer);
+            Console.WriteLine("slicingPlaneHeight:");
+            Console.WriteLine(slicingPlaneHeight);
+            
+            // Get paths according to slicing
+            var paths = SlicingAlgorithm(slicingPlaneHeight, triangleIndices, positions);
+            Console.WriteLine("paths: ");
+            Console.WriteLine(paths);
+
+            // Combine paths
+            var combinedPaths = ConnectLineSegments(paths);
+            Console.WriteLine("combinedPaths: ");
+            Console.WriteLine(combinedPaths);
+            // Adjust to line segments
+
+            return combinedPaths;
+        }
+
+        // ------
+
+        // --- Slicing Algorithm
+
+        private List<LineSegment> SlicingAlgorithm(double slicingPlaneHeight, List<int> triangleIndices, List<Point3D> positions)
+        {
+            var paths = new List<LineSegment>();
+            Debug.WriteLine(triangleIndices);
+            for (int i = 0; i < triangleIndices.Count; i += 3)
+            {
+                Point3D p1 = positions[triangleIndices[i]];
+                Point3D p2 = positions[triangleIndices[i + 1]];
+                Point3D p3 = positions[triangleIndices[i + 2]];
+                //check of triangle intersect met plane op height slicingPlaneHeight
+                Vector3D planeNormal = new Vector3D(0, 0, 1);
+                var intersections = FindTrianglePlaneIntersections(p1, p2, p3, planeNormal, slicingPlaneHeight);
+                if (intersections.Count == 3)
+                {
+
+                }
+                else if(intersections.Count == 2)
+                {
+                    var segment = new Data_Structures.LineSegment();
+                    segment.AddPoint(intersections[0]);
+                    segment.AddPoint(intersections[1]);
+                    paths.Add(segment);
+                }
+            }
+            return paths;
+        }
+
+        public static Point3D? IntersectLineSegmentWithPlane(Point3D p0, Point3D p1, Vector3D planeNormal, double planeD)
+        {
+            Vector3D direction = p1 - p0;
+            double dotProduct = Vector3D.DotProduct(planeNormal, direction);
+
+            if (Math.Abs(dotProduct) < 1e-6) // Parallel check
+            {
+                return null;
+            }
+
+            double t = -(Vector3D.DotProduct(planeNormal, p0.ToVector3D()) + planeD) / dotProduct;
+
+            if (t >= 0 && t <= 1) // Intersection within the segment
+            {
+                return p0 + t * direction;
+            }
+
+            return null;
+        }
+
+        public static List<Point3D> FindTrianglePlaneIntersections(Point3D v0, Point3D v1, Point3D v2, Vector3D planeNormal, double planeD)
+        {
+            var intersections = new List<Point3D>();
+
+            // Check each edge of the triangle
+            var i1 = IntersectLineSegmentWithPlane(v0, v1, planeNormal, planeD);
+            if (i1.HasValue) intersections.Add(i1.Value);
+
+            var i2 = IntersectLineSegmentWithPlane(v1, v2, planeNormal, planeD);
+            if (i2.HasValue) intersections.Add(i2.Value);
+
+            var i3 = IntersectLineSegmentWithPlane(v2, v0, planeNormal, planeD);
+            if (i3.HasValue) intersections.Add(i3.Value);
+
+            return intersections;
+        }
+
+
+
+        private double GetSlicingPlaneHeight(double zOffset, double layer)
+        {
+            return zOffset + (layer * SlicerSettings.LayerHeight);
+        }
+
+        // --- Path Combining Algorithm
+        
+        private PathsD ConnectLineSegments(List<LineSegment> lineSegments) {
+            if (lineSegments.Count == 0) return new PathsD(); // Check for empty list
+            
+            PathsD path = new PathsD(); // Make return variable
+
+            List<LineSegment> lineSegmentsCopy = new List<LineSegment>(lineSegments); // Copy list to work with
+            while (lineSegmentsCopy.Count > 0) {
+                PathD currentPath = new PathD(); // Make PathD to later add to PathsD return variable
+                LineSegment currentSegment = lineSegmentsCopy[0]; // Get the first segment in the list
+
+                PointD Start = currentSegment.GetPoints()[0]; // Get first PointD from current segment
+                PointD End = currentSegment.GetPoints()[^1]; // Get last PointD from current segment
+
+                currentPath.Add(Start); // Add Start and End PointD to currentPath
+                currentPath.Add(End);
+
+                lineSegmentsCopy.RemoveAt(0); // Remove the currentSegment from the list to make next segment be the first element for next loop
+
+                bool pathClosed = false;
+
+                while (!pathClosed && lineSegmentsCopy.Count > 0) {
+                    // Get the segment where the Start is the same as the current End.
+                    int nextSegmentIndex = lineSegmentsCopy.FindIndex(seg => seg.GetPoints()[0].Equals(End));
+
+                    // If next segment was found add the end of that 'new' currentSegment to the path and remove it from the list.
+                    if (nextSegmentIndex != -1) {
+                        currentSegment = lineSegmentsCopy[nextSegmentIndex];
+                        currentPath.Add(currentSegment.GetPoints()[currentSegment.GetPoints().Count - 1]);
+
+                        lineSegmentsCopy.RemoveAt(nextSegmentIndex);
+
+                        // If the first and last point are the same -> path is closed
+                        if (currentPath[0].Equals(currentPath[currentPath.Count - 1])) {
+                            pathClosed = true;
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+
+                path.Add(currentPath);
+            }
+
+            return path;
+        }
+
+        // ------
+
+        // --- Convert List<LineSegment> To PathD 
+
+        public PathD ConvertListToPathD(List<LineSegment> list)
+        {
+            var path = new PathD();
+
+            foreach (var segment in list)
+            {
+                path.AddRange(segment.GetPoints());
+            }
+
+            return path;
+        }
+
+        // ------
+
+        // --- Default Getter And Setters
+
+        public void SetGeometryModel3D(GeometryModel3D geometryModel)
+        {
+            geometryModel3D = geometryModel;
+        }  
+    
+        public GeometryModel3D GetGeometryModel3D()
+        {
+            return geometryModel3D;
+        }
+
+        public void SetSliderValue(double newValue) 
+        {
+            sliderValue = newValue;
+        }
+    
+        // ------
+    }
+}
